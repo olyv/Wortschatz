d310f29a4ecc93be52d01bc45b83907ae34c51eb
olyv <a.n.lyvin@gmail.com>
2016-06-08 22:33:41 +0200

    added answer counter

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -17,3 +17,32 @@ wortschatzApp.config(['$routeProvider',
         redirectTo: '/home'
       });
   }]);
+
+wortschatzApp.service('answerCounter',
+  function() {
+    var counter = 0;
+    var correctAnswers = 0;
+    var wrongAnswers = 0;
+
+    var getIncrementedCorrectAnswerCounter = function() {
+      correctAnswers = correctAnswers + 1;
+      return correctAnswers;
+    }
+    var getIncrementedWrongAnswerCounter = function() {
+      wrongAnswers = wrongAnswers + 1;
+      return wrongAnswers;
+    }
+    var getCorectAnswerCounter = function() {
+      return correctAnswers;
+    }
+    var getWrongAnswerCounter = function() {
+      return wrongAnswers;
+    };
+    
+    return {
+        getCorectAnswerCounter: getCorectAnswerCounter,
+        getWrongAnswerCounter: getWrongAnswerCounter,
+        getIncrementedWrongAnswerCounter: getIncrementedWrongAnswerCounter,
+        getIncrementedCorrectAnswerCounter: getIncrementedCorrectAnswerCounter
+    };
+});
--- app/assets/javascripts/lesson/lesson.js
+++ app/assets/javascripts/lesson/lesson.js
@@ -1,5 +1,5 @@
-wortschatzApp.controller('LessonCtrl', ['$scope',  'lessonItems', '$routeParams',
-  function($scope, lessonItems) {
+wortschatzApp.controller('LessonCtrl', ['$scope', '$uibModal', '$timeout', 'lessonItems', 'answerCounter', '$routeParams',
+  function($scope, $uibModal, $timeout, lessonItems, answerCounter) {
 
     $scope.active = 0;
     $scope.noWrapSlides = false;
@@ -11,10 +11,31 @@ wortschatzApp.controller('LessonCtrl', ['$scope',  'lessonItems', '$routeParams'
       });
     }
 
+    $scope.animationsEnabled = true;
+
+    $scope.open = function() {
+      var modalInstance = $uibModal.open({
+        animation: $scope.animationsEnabled,
+        size: "lg",
+        templateUrl: 'myModalContent.html',
+        controller: 'ModalInstanceCtrl',
+        resolve: {
+        correctAnswers: answerCounter.getCorectAnswerCounter(),
+        wrongAnswers: answerCounter.getWrongAnswerCounter()
+        }
+      });
+    };
+
     $scope.checkAnswerVerb = function(answerVerb) {
       //check if answer for verb is corect
       $scope.answerVerb.correct = ($scope.answerVerb.partizip == $scope.word.partizip) &&
                                   ($scope.answerVerb.auxverb == $scope.word.auxverb)
+
+      if ($scope.answerVerb.correct) {
+        $scope.counter = answerCounter.getIncrementedCorrectAnswerCounter();
+      } else {
+        $scope.counter = answerCounter.getIncrementedWrongAnswerCounter();
+      }
       //mark word as answered to display it as success or mistake (green or red)
       $scope.answerVerb.answered = true;
     };
@@ -24,6 +45,11 @@ wortschatzApp.controller('LessonCtrl', ['$scope',  'lessonItems', '$routeParams'
       $scope.answerNoun.correct = ($scope.answerNoun.plural == $scope.word.plural) &&
                                   ($scope.answerNoun.article == $scope.word.article)
 
+      if ($scope.answerNoun.correct) {
+        $scope.counter = answerCounter.getIncrementedCorrectAnswerCounter();
+      } else {
+        $scope.counter = answerCounter.getIncrementedWrongAnswerCounter();
+      }
       //mark word as answered to display it as success or mistake (green or red)
       $scope.answerNoun.answered = true;
     };
@@ -32,8 +58,22 @@ wortschatzApp.controller('LessonCtrl', ['$scope',  'lessonItems', '$routeParams'
       //check if answer for noun is corect
       $scope.answerAdjective.correct = ($scope.answerAdjective.translation == $scope.word.translation)
 
+      if ($scope.answerAdjective.correct) {
+        $scope.counter = answerCounter.getIncrementedCorrectAnswerCounter();
+      } else {
+        $scope.counter = answerCounter.getIncrementedWrongAnswerCounter();
+      }
       //mark word as answered to display it as success or mistake (green or red)
       $scope.answerAdjective.answered = true;
+    };
+
+    $scope.isLessonCompleted = function() {
+        // console.log(answerCounter.getCorectAnswerCounter() + " correct")
+        // console.log(answerCounter.getWrongAnswerCounter() + " wrong")
+        total = answerCounter.getWrongAnswerCounter() + answerCounter.getCorectAnswerCounter()
+        if (total == 9) {
+          $timeout(function(){$scope.open()}, 2500);
+        }
     };
 
     $scope.getInclude = function(type) {
@@ -47,9 +87,23 @@ wortschatzApp.controller('LessonCtrl', ['$scope',  'lessonItems', '$routeParams'
         return "lesson/shared/_adjective.html";
       }
     };
-
-
 }]);
+
+// $uibModalInstance represents a modal window (instance) dependency
+wortschatzApp.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, correctAnswers, wrongAnswers) {
+
+  $scope.correctAnswers = correctAnswers;
+  $scope.wrongAnswers = wrongAnswers;
+
+  $scope.ok = function () {
+    $uibModalInstance.close();
+    window.location.reload();
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
 
 wortschatzApp.factory('lessonItems', function($q, $timeout, $http) {
     var LessonItems = {
