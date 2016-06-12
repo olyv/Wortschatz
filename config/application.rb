fa1316e7637e3fc621d3a3f0d6ea243021830451
olyv <a.n.lyvin@gmail.com>
2016-06-12 19:14:38 +0200

    fix boottrap issue

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Wortschatz
   class Application < Rails::Application
 
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+    config.assets.paths << Rails.root.join('vendor', 'assets')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
