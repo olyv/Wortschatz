4426a78dcdf150b3cb0b26cbb0fad2a49b95cd4e
olyv <a.n.lyvin@gmail.com>
2016-06-13 08:28:30 +0200

    fix fonts

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Wortschatz
   class Application < Rails::Application
 
     config.assets.paths << Rails.root.join('vendor', 'assets')
+    config.assets.paths <<  Rails.root.join('/vendor/assets/bootstrap/fonts')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
